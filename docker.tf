db2e39bdb88cc76d8810931a348c11a0de5e4574
Nagendra Kadumuru <nagendrakadumuru83@gmail.com>
2025-06-18 16:16:36 +0530

    expense infra

--- docker.tf
+++ docker.tf
@@ -2,6 +2,13 @@ resource "aws_instance" "this" {
     ami = "ami-09c813fb71547fc4f" # This is our devops-practice AMI ID
     vpc_security_group_ids = [aws_security_group.allow_all_docker.id]
     instance_type = "t3.micro"
+
+    # 20GB is not enough
+    root_block_device {
+      volume_size = 50 #Set root volume size to 50GB
+      volume_type = "gp3" #Use gp3 for better performance (optional)
+    }
+    user_data = file("docker.sh")
     tags = {
         Name = "docker"
     }
@@ -42,4 +49,8 @@ resource "aws_security_group" "allow_all_docker" {
     tags = {
         Name = "allow_tls"
     }
+}
+
+output "docker_ip" {
+  value = aws_instance.this.public_ip
 }
